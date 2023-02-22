1c0b36f331a103834bb9eb3a1053f2633a0add0e
Amutheswaran <amutheswaran@outlook.com>
2023-02-22 20:26:39 +0530

    first commit

---
+++ src/main/java/com/ammu/controller/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.ammu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+	@GetMapping("/greet")
+	public String sayHello() {
+		return "<h1>Hello, World!</h1>";
+	}
+	
+}
